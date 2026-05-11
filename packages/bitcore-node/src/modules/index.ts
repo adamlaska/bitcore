40ae54c736a9247afd60e68ac2037d1514ace5b0
Micah Maphet <micahmaphet@gmail.com>
2026-05-11 13:08:36 -0400

    fix module path overwrite bug

--- packages/bitcore-node/src/modules/index.ts
+++ packages/bitcore-node/src/modules/index.ts
@@ -18,13 +18,13 @@ export function loadModules(params: Partial<ChainNetwork> = {}) {
 
   // Auto register known modules from config.chains
   for (const chain of chains) {
-    let modulePath = DEFAULT_MODULE_PATHS[chain];
+    const defaultModulePath = DEFAULT_MODULE_PATHS[chain];
 
     // Register for each
     const networks = params.network ? [params.network] : Config.networksFor(chain);
     for (const network of networks) {
       const config = Config.chainConfig({ chain, network });
-      modulePath = config.module || modulePath; // custom module path
+      const modulePath = config.module || defaultModulePath; // custom module path
       if (!modulePath) {
         logger.warn(`Module not found for ${chain}:${network}. Did you forget to specify 'module' in the config?`);
         continue;
